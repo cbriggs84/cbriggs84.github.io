5bafb4472c6b702459c8de10033c6f4cfe622fec
cbriggs84 <cbriggs84@aol.com>
2023-03-01 11:05:36 -0500

    - login comp

---
+++ nobetween/src/app/auth/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
+
+@Component({
+	standalone: true,
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
+	imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+	encapsulation: ViewEncapsulation.None,
+})
+export class LoginComponent implements OnInit
+{
+	formGroup = new UntypedFormGroup({});
+
+	constructor() { }
+
+	ngOnInit(): void
+	{
+		this.formGroup.addControl('Email', new FormControl());
+		this.formGroup.addControl('Password', new FormControl());
+	}
+
+}
